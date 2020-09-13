937dc5d926087c368bad63f45976b177983aa5ee
saurav <saurav.sharma.zz45@gmail.com>
2020-09-13 09:44:44 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("\n\n\n=======================================================================")
+print("||===================|| HEALTHY PROGRAMMER ||=======================||")
+print("=======================================================================")
+print("\nthis program will remind you to drink water and do eyes-exercise every 30 \nand physical exercise every 45 minutes \nPlease don't close the program. Minimise it, so that it can run in the\nbackground...")
